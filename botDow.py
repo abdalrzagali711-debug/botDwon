8ae64296d5972b7d95f15faf52e77f86bcfe7027
abdalrzagali711-debug <abdalrzagali711@gmail.com>
2026-04-10 21:57:05 +0300

    Change bot token in botDow.py
    Updated the bot token to a new value.

--- botDow.py
+++ botDow.py
@@ -7,7 +7,7 @@ from flask import Flask
 from threading import Thread
 
 # --- إعدادات البوت وقاعدة البيانات ---
-TOKEN = "7954952627:AAEXIZerk_CRxI940lWq98RY0gHLSPI1wu4"
+TOKEN = "7954952627:AAHAVoD3ZFD56TiSqkGdbymUzrc0setkpo4"
 MONGO_URI = "mongodb+srv://abdalrzagDB:10010207966##@cluster0.fighoyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 ADMIN_ID = 5524416062 
 
